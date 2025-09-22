0c34b15a2e91ef66ec75950393d31b9feccffe44
PEzwarts <pe.zwarts@tutamail.com>
2025-09-22 22:10:46 +0000

    Update config; Needed to change opts.lua to options.lua due to neovide settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@ vim.g.autoformat = true
 
 -- vim.g.neovide_remember_window_size = true
 -- vim.g.neovide_cursor_hack = true
--- vim.g.neovide_opacity = 0.8
+vim.g.neovide_opacity = 0.8
 -- vim.g.neovide_cursor_animation_length = 0.03
 -- vim.g.neovide_scroll_animation_length = 0
 
