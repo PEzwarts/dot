cc8c7ed8dc6e836c4671ca7d076f1b73c7144a4b
PEzwarts <pe.zwarts@tutamail.com>
2025-09-22 21:56:22 +0000

    Update config

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -6,7 +6,8 @@ vim.g.autoformat = true
 -- vim.g.neovide_cursor_animation_length = 0.03
 -- vim.g.neovide_scroll_animation_length = 0
 
-vim.g.neovide_scale_factor = 0.45
+-- vim.g.neovide_scale_factor = 0.45
+vim.g.neovide_scale_factor = 1
 
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_scroll_animation_length = 0.1
