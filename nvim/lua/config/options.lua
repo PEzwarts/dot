48e2b7c214d592b356fe2702388dc3dd874bb4bf
PEzwarts <pe.zwarts@tutamail.com>
2025-09-23 10:53:13 +0000

    Update config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,7 +7,7 @@ vim.g.neovide_opacity = 0.8
 -- vim.g.neovide_scroll_animation_length = 0
 
 -- vim.g.neovide_scale_factor = 0.45
-vim.g.neovide_scale_factor = 1
+vim.g.neovide_scale_factor = 0.8
 
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_scroll_animation_length = 0.1
