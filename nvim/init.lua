36cedc057a57f69455c4d71c1813c9b83c8a2a69
PEzwarts <pe.zwarts@tutamail.com>
2025-09-24 13:34:11 +0000

    Update dot

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,12 @@ require("config.lazy")
 
 vim.cmd(":set nohidden")
 
+vim.g.autoformat = true
+vim.g.neovide_opacity = 0.8
+vim.g.neovide_scale_factor = 0.6
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_scroll_animation_length = 0.1
+
 vim.g.term = true
 vim.g.write = false
 
