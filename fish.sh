f91a441812373c1d4139373772d85f2869447457
PEzwarts <pe.zwarts@tutamail.com>
2025-09-28 03:14:09 +0000

    Update dot

--- fish.sh
+++ fish.sh
@@ -1,11 +1,11 @@
 if [ "$(uname)" == "linux" ]; then
-  if [ "$(echo $SHELL)" == "bash" ]; then
+  if [ "$(echo $SHELL)" == "/bin/bash" ]; then
     echo /usr/bin/fish | sudo tee -a /etc/shells
 
     chsh -s /usr/bin/fish
   fi
 else
-  if [ "$(echo $SHELL)" == "bash" ]; then
+  if [ "$(echo $SHELL)" == "/bin/zsh" ]; then
     echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 
     chsh -s /opt/homebrew/bin/fish
