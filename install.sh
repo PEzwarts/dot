2ebe056b807e962791a2e1130bc73d71fe8816d8
PEzwarts <pe.zwarts@tutamail.com>
2025-09-28 04:52:33 +0000

    Update dot

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ else
   # settings="/Applications/System Settings.app"
   # neovide="/opt/homebrew/Cellar/neovide/0.15.2/Neovide.app"
 
-  declare -a apps=("/System/Applications/'System Settings'.app" "/opt/homebrew/Cellar/neovide/0.15.2/Neovide.app")
+  declare -a apps=("/Applications/System Settings.app" "/opt/homebrew/Cellar/neovide/0.15.2/Neovide.app")
 
   for app in ${apps[@]}; do
     defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
