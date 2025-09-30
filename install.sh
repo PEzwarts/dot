161bba06de65efa6df2a98325b9a265162a3bdbb
PEzwarts <pe.zwarts@tutamail.com>
2025-09-30 21:51:58 +0000

    Update dot

--- install.sh
+++ install.sh
@@ -71,34 +71,6 @@ else
   # Applications
   brew install libreoffice
 
-  defaults write com.apple.dock persistent-others -array
-  defaults write com.apple.dock persistent-apps -array
-  defaults write com.apple.dock recents-apps -array
-
-  defaults write com.apple.dock tilesize -int 40
-  defaults write com.apple.dock orientation -string bottom
-
-  # declare -a apps=("/opt/homebrew/Cellar/Neovide/0.15.2/Neovide.app" "/Applications/Safari.app")
-  declare -a apps=("/Applications/Alacritty.app" "/Applications/Safari.app")
-
-  for app in ${apps[@]}; do
-    defaults write com.apple.dock persistent-apps -array-add "
-    <dict>
-      <key>tile-data</key>
-      <dict>
-        <key>file-data</key>
-        <dict>
-          <key>_CFURLString</key>
-          <string>$app</string>
-          <key>_CFURLStringType</key>
-          <integer>0</integer>
-        </dict>
-      </dict>
-    </dict>"
-  done
-
-  killall Dock
-
   /Applications/Amethyst.app/Contents/MacOS/Amethyst & disown
 
   ./update.sh
