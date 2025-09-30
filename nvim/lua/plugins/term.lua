c20b41adb5b4717cbde5b99ba761934b53771798
PEzwarts <pe.zwarts@tutamail.com>
2025-09-30 21:53:57 +0000

    Update dot

--- nvim/lua/plugins/term.lua
+++ nvim/lua/plugins/term.lua
@@ -5,8 +5,6 @@ return {
     opts = {
       direction = "float",
 
-      persist_size = false,
-
       float_opts = {
         width = vim.api.nvim_win_get_width(0),
         height = vim.api.nvim_win_get_height(0)
