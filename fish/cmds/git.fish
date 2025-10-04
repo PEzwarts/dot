056d8daa9be2ec5d6a7b3991851187ef5b4dab3e
PEzwarts <pe.zwarts@tutamail.com>
2025-10-04 18:38:38 +0000

    Update dot

--- fish/cmds/git.fish
+++ fish/cmds/git.fish
@@ -14,27 +14,28 @@ alias gcm "git commit -m"
 alias gcma "git commit --amend -m"
 # alias gr "git revert"
 
+alias gph "git reset --hard && git pull"
+
 alias gP "git push"
 alias gp "git pull"
 alias gf "git fetch"
 
 # Rebase dangerous; changes git hashes.
-# alias grb "git rebase "
+# alias grb "git rebase"
 
 # Unknown havent tried yet.
 # alias gs "git switch "
 # alias gr "git reset "
 # alias gR "git restore"
+# alias gta "git worktree add"
+# alias gtr "git worktree remove"
+# alias gtl "git worktree list"
 
 alias gc "git clone"
 alias gd "git diff | cat"
 alias gl "git log --graph --oneline --decorate | cat"
 
 alias gco "git checkout"
-
-# alias gta "git worktree add"
-# alias gtr "git worktree remove"
-# alias gtl "git worktree list"
 
 alias grma "git remote add"
 alias grmr "git remote remove"
