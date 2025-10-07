792926416e0bdd1987dd1e41eb8e3f0ff7c82c4f
PEzwarts <pe.zwarts@tutamail.com>
2025-10-07 17:26:31 +0000

    Update dot

--- install.sh
+++ install.sh
@@ -32,8 +32,7 @@ if [ -f /usr/bin/hyprctl ]; then
   sudo pacman -S --noconfirm fish eza dysk fastfetch fd ripgrep fzf ranger
 
   # Wallpapers
-  cp -r ./wall/ ~/Desktop/
-  # git clone https://github.com/elementary/wallpapers ~/Downloads/wallpapers/
+  git clone https://github.com/PEzwarts/wall ~/Desktop/wall/
 
   # Applications
   sudo pacman -S --noconfirm libreoffice-fresh anki
