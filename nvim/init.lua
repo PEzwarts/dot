eb1b237f0d1d26fae89bb74ce809f4e0854813b7
PEzwarts <pe.zwarts@tutamail.com>
2025-10-07 17:35:14 +0000

    Update dot

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,7 @@ vim.g.autoformat = false
 vim.g.neovide_opacity = 0.8
 vim.g.neovide_scale_factor = 0.6
 vim.g.neovide_cursor_animation_length = 0
-vim.g.neovide_scroll_animation_length = 0.05
+vim.g.neovide_scroll_animation_length = 0.1
 
 vim.g.term = true
 vim.g.write = false
