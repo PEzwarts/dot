4bda8c7bafc6774310fdf2e5134f5d367d62e5cd
PEzwarts <pe.zwarts@tutamail.com>
2025-10-15 20:39:55 +0000

    Update dot

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,10 @@
 require("config.lazy")
 
--- vim.api.nvim_create_autocmd("SessionLoadPost", {
---   callback = function ()
---     if vim.api.nvim_win_get_width(0) > 300 then
---       vim.cmd(":TZAtaraxis")
---     end
---   end
--- })
+vim.api.nvim_create_autocmd("SessionLoadPost", {
+  callback = function ()
+    vim.cmd(":Neotree action=show position=current")
+  end
+})
 
 vim.api.nvim_create_autocmd("BufDelete", {
   callback = function ()
