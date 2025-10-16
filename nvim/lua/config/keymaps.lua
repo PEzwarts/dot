1cfaf2cc43c432eb22825a1a59d97150ca52260a
PEzwarts <pe.zwarts@tutamail.com>
2025-10-16 12:54:12 +0000

    Update dot

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,12 +3,12 @@ local opts = { remap = true, silent = true }
 
 -- Neotree
 
--- keymap.set({ "n", "i", "v" }, "<D-c>", "<cmd>:Neotree toggle position=current<cr>", opts)
+keymap.set({ "n", "i", "v" }, "<D-c>", "<esc><cmd>:Neotree toggle position=current<cr>", opts)
 
-keymap.set({ "n", "i", "v" }, "<D-c>", function ()
-  vim.cmd(":Neotree toggle position=current")
-  vim.cmd(":wincmd w")
-end, opts)
+-- keymap.set({ "n", "i", "v" }, "<D-c>", function ()
+--   vim.cmd(":Neotree toggle position=current")
+--   vim.cmd(":wincmd w")
+-- end, opts)
 
 -- Close an buffer
 
