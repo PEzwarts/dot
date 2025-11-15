aef84e96d3f924dd44831eff24c4ab59155ab1ed
PEzwarts <pe.zwarts@tutamail.com>
2025-11-15 15:27:44 +0000

    Update dot

--- all/nvim/lua/config/options.lua
+++ all/nvim/lua/config/options.lua
@@ -8,6 +8,12 @@ vim.g.neovide_scale_factor = 0.7
 vim.g.neovide_cursor_animation_length = 0.02
 vim.g.neovide_scroll_animation_length = 0.06
 
+vim.g.neovide_position_animation_length = 0
+
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_animate_command_line = false
+vim.g.neovide_scroll_animation_far_lines = 0
+
 vim.g.neovide_padding_left = 400
 vim.g.neovide_padding_right = 400
 
