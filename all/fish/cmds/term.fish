707b04e47e8133a61b2edee44e76ed8ef8223101
PEzwarts <pe.zwarts@tutamail.com>
2025-11-17 21:20:55 +0000

    Update dot

--- all/fish/cmds/term.fish
+++ all/fish/cmds/term.fish
@@ -14,4 +14,20 @@ function v
     end
 end
 
+function t
+    if [ "$(uname)" = Linux ]
+        if [ (hyprctl -j activeworkspace | jq -r '.windows') = 0 ]
+            neovide -- -c ":lua vim.g.term = false vim.g.neovide_padding_left = 500 vim.g.neovide_padding_right = 500"
+        else if [ (hyprctl -j activeworkspace | jq -r '.windows') > 1 ]
+            neovide -- -c ":lua vim.g.term = false vim.g.neovide_padding_left = 0 vim.g.neovide_padding_right = 0"
+        end
+
+        bash -c "sleep 0.1 && hyprctl dispatch resizeactive -450 0" & disown
+        hyprctl dispatch killactive
+    else
+        killall neovide
+        neovide -- -c ":lua vim.g.term = false" & disown
+    end
+end
+
 export EDITOR=nvim
