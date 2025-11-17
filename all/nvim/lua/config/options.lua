a614fd7883a01419cdb4f637427bb86fc38ed482
PEzwarts <pe.zwarts@tutamail.com>
2025-11-17 22:44:28 +0000

    Update dot

--- all/nvim/lua/config/options.lua
+++ all/nvim/lua/config/options.lua
@@ -14,4 +14,7 @@ vim.g.neovide_cursor_animate_in_insert_mode = false
 vim.g.neovide_cursor_animate_command_line = false
 vim.g.neovide_scroll_animation_far_lines = 0
 
+vim.g.neovide_padding_left = 500
+vim.g.neovide_padding_right = 500
+
 vim.g.term = true
