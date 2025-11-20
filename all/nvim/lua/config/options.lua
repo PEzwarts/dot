74dadc6e9c17417f1633f62973294f981050359b
PEzwarts <pe.zwarts@tutamail.com>
2025-11-20 17:13:48 +0000

    Update dot

--- all/nvim/lua/config/options.lua
+++ all/nvim/lua/config/options.lua
@@ -8,14 +8,14 @@ vim.opt.smarttab = true
 vim.opt.expandtab = true
 vim.opt.confirm = false
 
-vim.opt.hidden = false
+vim.opt.hidden = true
 vim.opt.swapfile = false
 
 vim.wo.relativenumber = false
 
 vim.g.neovide_scale_factor = 0.7
 
-vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0.02
 vim.g.neovide_scroll_animation_length = 0.06
 
 vim.g.neovide_position_animation_length = 0
