07f1764f32918f9a2b48acdb349379d5854dbc86
PEzwarts <pe.zwarts@tutamail.com>
2025-11-22 15:29:58 +0000

    Update dot

--- all/nvim/lua/plugins/clr.lua
+++ all/nvim/lua/plugins/clr.lua
@@ -9,9 +9,9 @@ return {
 
   { "RRethy/base16-nvim" },
 
-  -- { "Kaikacy/Lemons.nvim" },
+  { "Kaikacy/Lemons.nvim" },
 
-  -- { "makestatic/devel.nvim" }
+  { "makestatic/devel.nvim" }
 
   -- { "xiyaowong/transparent.nvim" }
 }
--- all/nvim/lua/plugins/select.lua
+++ all/nvim/lua/plugins/select.lua
@@ -13,6 +13,7 @@ return {
 
       projects = {
         "/run/media/overlord/d0/*",
+        "~/Desktop/*"
       },
     },
 
