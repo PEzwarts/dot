389d848e740f9d6b24d601ac391b4077c93d7f83
PEzwarts <pe.zwarts@tutamail.com>
2025-11-22 15:18:15 +0000

    Update dot

--- all/nvim/lua/plugins/move.lua
+++ all/nvim/lua/plugins/move.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "arnamak/stay-centered.nvim",
+
+    config = function()
+      require("stay-centered").setup({
+        allow_scroll_move = true,
+      })
+    end
+  },
+
+  {
     "fedepujol/move.nvim",
 
     opts = {}
