0a203547b435903b4d102fdeea9cb1a5e76f580c
PEzwarts <pe.zwarts@tutamail.com>
2025-12-08 12:52:54 +0000

    Update dot

--- all/nvim/lua/config/options.lua
+++ all/nvim/lua/config/options.lua
@@ -16,7 +16,7 @@ vim.wo.relativenumber = false
 if vim.g.neovide then
   vim.g.neovide_scale_factor = 0.7
 
-  vim.g.neovide_cursor_animation_length = 0.02
+  vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_scroll_animation_length = 0.02
 
   vim.g.neovide_position_animation_length = 0
