f86fb23f77bf135f60c3e2895075ea11ee538032
PEzwarts <pe.zwarts@tutamail.com>
2025-12-12 20:46:01 +0000

    Update dot

--- all/fish/cmds/term.fish
+++ all/fish/cmds/term.fish
@@ -1,8 +1,8 @@
 function v
     if [ "$(uname)" = Linux ]
-        # alacritty -e nvim -c ":lua vim.g.term = false" & disown
+        alacritty -e nvim -c ":lua vim.g.term = false" & disown
         # neovide -- -c ":lua vim.g.term = false" & disown
-        ghostty -e nvim -c ":lua vim.g.term = false" & disown
+        # ghostty -e nvim -c ":lua vim.g.term = false" & disown
 
         bash -c "sleep 0.1 && hyprctl dispatch resizeactive -450 0" & disown
         hyprctl dispatch killactive
