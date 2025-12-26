0cf7c9ec57a1505d79c3e2933cf52b5d10fe8af5
PEzwarts <pe.zwarts@mailbox.org>
2025-12-26 19:23:55 +0000

    Update dot; term switch

--- all/nvim/lua/plugins/clr.lua
+++ all/nvim/lua/plugins/clr.lua
@@ -3,18 +3,13 @@ return {
     "LazyVim/LazyVim",
 
     opts = {
-      colorscheme = "base16-3024"
-      -- colorscheme = "devel"
-      -- colorscheme = "Lemons"
+      -- colorscheme = "base16-3024"
+      colorscheme = "base16-gruvbox-dark"
       -- colorscheme = "elflord"
     }
   },
 
   { "RRethy/base16-nvim" },
-
-  { "Kaikacy/Lemons.nvim" },
-
-  { "makestatic/devel.nvim" }
 
   -- { "xiyaowong/transparent.nvim" }
 }
