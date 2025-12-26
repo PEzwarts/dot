a66d646573c27cfc1f3a89fe0ac121f75676fcb5
PEzwarts <pe.zwarts@mailbox.org>
2025-12-26 18:35:26 +0000

    Update dot; telescope patch

--- all/nvim/lua/config/options.lua
+++ all/nvim/lua/config/options.lua
@@ -20,7 +20,7 @@ vim.g.floaterm_borderchars = " "
 vim.wo.relativenumber = false
 
 if vim.g.neovide then
-  vim.g.neovide_scale_factor = 0.7
+  vim.g.neovide_scale_factor = 0.65
 
   vim.g.neovide_cursor_animation_length = 0.02
   vim.g.neovide_scroll_animation_length = 0.02
--- all/nvim/lua/plugins/select.lua
+++ all/nvim/lua/plugins/select.lua
@@ -1,21 +1,5 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            width = { padding = 120 },
-            height = { padding = 5 },
-            preview_width = 0.5,
-          },
-        },
-      },
-    },
-  },
+  { "nvim-telescope/telescope.nvim" },
 
   {
     "coffebar/neovim-project",
