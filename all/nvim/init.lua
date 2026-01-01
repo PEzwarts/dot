b782dd03487ec7e044b81b33fbd5eb1c82156995
PEzwarts <pe.zwarts@mailbox.org>
2026-01-01 16:36:18 +0000

    Update dot; term.lua

--- all/nvim/init.lua
+++ all/nvim/init.lua
@@ -1,3 +1,13 @@
 require("config.lazy")
 
+vim.cmd(":set nonumber")
+vim.cmd(":set nocursorline")
+
 vim.cmd(":NoNeckPain")
+
+vim.api.nvim_create_autocmd("SessionLoadPost", {
+  callback = function()
+    vim.cmd(":set number")
+    vim.cmd(":set cursorline")
+  end
+})
