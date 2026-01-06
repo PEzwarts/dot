2b4fbc1b0c2c2e7aa3f40015714fc9769498ef9d
PEzwarts <pe.zwarts@mailbox.org>
2026-01-06 16:30:58 +0000

    Update dot; hyprland

--- all/fish/cmds/term.fish
+++ all/fish/cmds/term.fish
@@ -1,7 +1,7 @@
 function v
     if [ "$(uname)" = Linux ]
         # alacritty -e nvim -c ":lua vim.g.term = false" & disown
-        neovide -- -c ":lua vim.g.term = false" & disown
+        neovide --no-vsync -- -c ":lua vim.g.term = false" & disown
         # rio -e nvim -c ":lua vim.g.term = false" & disown
 
         hyprctl dispatch killactive
