5201534da0993344e3e5fcb4298fb3b869333462
PEzwarts <pe.zwarts@mailbox.org>
2026-01-06 22:23:25 +0000

    Update dot; options.lua

--- all/nvim/lua/config/options.lua
+++ all/nvim/lua/config/options.lua
@@ -28,7 +28,7 @@ if vim.g.neovide then
   vim.g.neovide_position_animation_length = 0
 
   vim.g.neovide_cursor_animate_in_insert_mode = true
-  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_animate_command_line = false
   vim.g.neovide_scroll_animation_far_lines = 0
 
   vim.g.neovide_cursor_trail_size = 0
