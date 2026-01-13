46f73c6855b3df226ad5d651eaa6ef02f99e9c3b
PEzwarts <pe.zwarts@mailbox.org>
2026-01-13 00:16:02 +0000

    Update dot; config.fish

--- os/lin/arch/x86_64/conf/fish/config.fish
+++ os/lin/arch/x86_64/conf/fish/config.fish
@@ -8,10 +8,10 @@ source ~/.config/fish/cmds/rst.fish
 
 source ~/.config/fish/cmds/py.fish
 
-# source ~/.config/fish/cmds/brew.fish
+source ~/.config/fish/cmds/gpg.fish
+source ~/.config/fish/cmds/gnu.fish
 source ~/.config/fish/cmds/git.fish
 
-source ~/.config/fish/cmds/gnu.fish
 source ~/.config/fish/cmds/term.fish
 
 # export VCPKG_MAX_CONCURRENCY=4
