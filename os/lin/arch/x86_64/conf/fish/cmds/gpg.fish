4cd6f70517f5f875ec07cab677e0a1bc55a57b30
PEzwarts <pe.zwarts@mailbox.org>
2026-01-13 01:27:53 +0000

    Update dot; gpg.fish

--- os/lin/arch/x86_64/conf/fish/cmds/gpg.fish
+++ os/lin/arch/x86_64/conf/fish/cmds/gpg.fish
@@ -1,6 +1,11 @@
-alias gpgi "gpg --expert --full-generate-key"
+alias gpgi "gpg --full-generate-key"
+
 alias gpge "gpg -e"
 alias gpgd "gpg -d"
+
 alias gpgs "gpg --sign"
+
+alias gpgl "gpg -k"
+
 alias gpgim "gpg --import --armor"
 alias gpgex "gpg --export --armor"
