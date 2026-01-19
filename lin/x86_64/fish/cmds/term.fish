0039e178995ad7c341b39a8078f7cb3435d4a524
PEzwarts <pe.zwarts@mailbox.org>
2026-01-19 00:05:57 +0000

    Update dot; nvim

--- lin/x86_64/fish/cmds/term.fish
+++ lin/x86_64/fish/cmds/term.fish
@@ -1,16 +1,16 @@
 function v
     if [ "$(uname)" = Linux ]
-        # alacritty -e nvim -c ":lua vim.g.term = false" & disown
         neovide -- -c ":lua vim.g.term = false" & disown
+        # alacritty -e nvim -c ":lua vim.g.term = false" & disown
         # rio -e nvim -c ":lua vim.g.term = false" & disown
 
         hyprctl dispatch killactive
     else
-        # killall alacritty
-        # alacritty -e nvim -c ":lua vim.g.term = false"
-
         killall neovide
         neovide -- -c nvim -c ":lua vim.g.term = false"
+
+        # killall alacritty
+        # alacritty -e nvim -c ":lua vim.g.term = false"
 
         # killall rio
         # rio -e nvim -c ":lua vim.g.term = false"
