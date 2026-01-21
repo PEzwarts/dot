eb8516114f6cbd760d638cabd865d309e6a28917
PEzwarts <pe.zwarts@mailbox.org>
2026-01-21 23:00:43 +0000

    Update dot; nvim

--- lin/nvim/lua/plugins/clr.lua
+++ lin/nvim/lua/plugins/clr.lua
@@ -3,9 +3,9 @@ return {
     "LazyVim/LazyVim",
 
     opts = {
-      -- colorscheme = "base16-3024"
+      colorscheme = "base16-3024"
       -- colorscheme = "darkbox"
-      colorscheme = "elflord"
+      -- colorscheme = "elflord"
     }
   },
 
