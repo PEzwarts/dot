30f299d2072fc3cc880a1c0e511cb39169017f4a
PEzwarts <pe.zwarts@mailbox.org>
2026-02-02 20:43:46 +0000

    Update dot; NoNeckPain return

--- lin/fish/cmds/nvim.fish
+++ lin/fish/cmds/nvim.fish
@@ -1,4 +1,7 @@
 function v
   nvr --remote-send "<C-\><C-n><cmd>:ToggleTerm<cr>"
+  # nvr --remote-send "<cmd>:set number<cr>"
+  # nvr --remote-send "<cmd>:set cursorline<cr>"
+  # nvr --remote-send "<cmd>:Neominimap Enable<cr>"
   nvr --remote-send "<cmd>:NeovimProjectDiscover<cr>"
 end
--- lin/nvim/init.lua
+++ lin/nvim/init.lua
@@ -1,7 +1,8 @@
 require("config.lazy")
 
--- vim.cmd(":ToggleTerm")
--- vim.cmd(":NoNeckPain")
+vim.cmd(":set number")
+vim.cmd(":set cursorline")
+vim.cmd(":Neominimap Enable")
 
 if vim.g.neovide then
   require("smear_cursor").enabled = false
@@ -13,13 +14,6 @@ vim.api.nvim_create_autocmd("SessionLoadPost", {
   callback = function()
     local keymap = vim.keymap
     local opts = { remap = true, silent = true }
-
-    -- vim.cmd(":ToggleTerm")
-    -- vim.cmd(":NoNeckPain")
-
-    -- vim.cmd(":set number")
-    -- vim.cmd(":set cursorline")
-    -- vim.cmd(":Neominimap Enable")
 
     keymap.set({ "n", "i", "v" }, "<A-s>", "<esc><cmd>:NeovimProjectDiscover<cr>", opts)
 
@@ -42,25 +36,20 @@ vim.api.nvim_create_autocmd("SessionLoadPost", {
   end
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd("BufNew", {
   callback = function()
     -- local buf = vim.api.nvim_get_current_buf()
     --
     -- if not vim.bo[buf].buftype == "terminal" then
-    --   vim.cmd(":set nonumber")
-    --   vim.cmd(":set nocursorline")
-    --   vim.cmd(":Neominimap Disable")
+    --   vim.cmd(":set number")
+    --   vim.cmd(":set cursorline")
+    --   vim.cmd(":Neominimap Enable")
     -- end
   end
 })
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    -- vim.cmd(":set nonumber")
-    vim.cmd(":set nocursorline")
-    -- vim.cmd(":Neominimap Disable")
-
-    vim.cmd(":ToggleTerm")
     vim.cmd(":NoNeckPain")
   end
 })
