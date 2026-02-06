362ad0e09d5c55505aef68e546fb5ef7b7b27cb8
PEzwarts <pe.zwarts@mailbox.org>
2026-02-06 21:29:20 +0000

    Update dot; select.lua

--- lin/nvim/lua/plugins/select.lua
+++ lin/nvim/lua/plugins/select.lua
@@ -18,8 +18,25 @@ return {
             preview_width = 0.4,
           },
         },
+
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
       },
     },
+  },
+
+  {
+    "nvim-telescope/telescope-fzf-native.nvim", build = "make",
+
+    config = function()
+      require("telescope").load_extension("fzf")
+    end
   },
 
   {
