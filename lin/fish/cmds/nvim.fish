e6b0779518b479ad5001c52bee72a6682f623150
PEzwarts <pe.zwarts@mailbox.org>
2026-02-06 21:30:15 +0000

    Update dot; nvim.fish

--- lin/fish/cmds/nvim.fish
+++ lin/fish/cmds/nvim.fish
@@ -1,8 +1,8 @@
 function v
-  nvr --remote-send "<cmd>:lua base()<cr>"
-  nvr --remote-send "<cmd>:lua keys()<cr>"
   nvr --remote-send "<cmd>:NoNeckPain<cr>"
   nvr --remote-send "<C-\><C-n><cmd>:ToggleTerm<cr>"
   nvr --remote-send "<cmd>:NoNeckPain<cr>"
   nvr --remote-send "<cmd>:NeovimProjectDiscover<cr>"
+  nvr --remote-send "<cmd>:lua base()<cr>"
+  nvr --remote-send "<cmd>:lua keys()<cr>"
 end
